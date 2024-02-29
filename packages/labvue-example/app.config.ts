f087381a737ae5d90cc191c58fdb59a2ea76bb7b
SelfhostedPro <info@selfhosted.pro>
2024-02-29 12:56:55 -0800

    init

---
+++ packages/labvue-example/app.config.ts
@@ -0,0 +1,5 @@
+export default defineAppConfig({
+    myLayer: {
+      name: 'My amazing Example layer (overwritten)'
+    }
+  })
---
+++ packages/labvue-layer/vuetify.config.ts
@@ -0,0 +1,6 @@
+// vuetify.config.ts
+import { defineVuetifyConfiguration } from 'vuetify-nuxt-module/custom-configuration'
+
+export default defineVuetifyConfiguration({
+    
+})
